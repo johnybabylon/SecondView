04246b5d10ee4c3af24179cd331879bf90586ada
johny babylon <ztredded@gmail.com>
2015-12-24 01:50:10 -0800

    finally figured out CollectionViews!!!!

--- NewsController.h
+++ NewsController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  NewsController.h
 //  SecondView
 //
 //  Created by elliott chavis on 12/24/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface NewsController : UICollectionViewController
 
 @end
-
